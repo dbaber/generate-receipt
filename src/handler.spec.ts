3aea0b2d6a83842c58917bcbeac7cd1c34ba6003
Dan Baber <daniel.baber@xome.com>
2020-03-02 12:55:20 -0600

    Add serverless generated template files and Hello World lambda

---
+++ src/handler.spec.ts
@@ -0,0 +1,18 @@
+// handler.spec.ts
+import 'mocha';
+import { expect } from 'chai';
+import 'chai/register-should';
+//import { APIGatewayEvent, Handler, Callback, Context } from 'aws-lambda';
+//import { hello } from '../handler';
+import { hello } from './handler';
+
+describe("handler", () => {
+  describe("hello", () => {
+    it("should return Serverless boilerplate message", () => {
+      hello(null, null, (error : Error, result : any) => {
+        expect(error).to.be.null;
+        result.body.should.equal('{"message":"Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!","input":null,"context":null}');
+      })
+    });
+  });
+});
