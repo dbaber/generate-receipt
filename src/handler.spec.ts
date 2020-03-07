d7bc4bedb0467ecbe8755d8ada7d9c01adb7214b
Dan Baber <daniel.baber@xome.com>
2020-03-07 10:29:02 -0600

    Clean up some things and turn on strictNullChecks

--- src/handler.spec.ts
+++ src/handler.spec.ts
@@ -2,14 +2,23 @@
 import 'mocha';
 import { expect } from 'chai';
 import 'chai/register-should';
-//import { APIGatewayEvent, Handler, Callback, Context } from 'aws-lambda';
-//import { hello } from '../handler';
-import { hello } from './handler';
+import { hello, generate_receipt } from './handler';
 
 describe("handler", () => {
   describe("hello", () => {
     it("should return Serverless boilerplate message", () => {
       hello(null, null, (error : Error, result : any) => {
+        expect(error).to.be.null;
+        result.body.should.equal('{"message":"Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!","input":null,"context":null}');
+      })
+    });
+  });
+});
+
+describe("handler", () => {
+  describe("generate_receipt", () => {
+    it("should return Serverless boilerplate message", () => {
+      generate_receipt(null, null, (error : Error, result : any) => {
         expect(error).to.be.null;
         result.body.should.equal('{"message":"Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!","input":null,"context":null}');
       })
